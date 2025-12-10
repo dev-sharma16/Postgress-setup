9a38bd7f02efb506d5bf1a03bbd4c6d17eaa2a3c
Dev Sharma <devsharma1683@gmail.com>
2025-12-10 20:50:22 +0530

    added an endpoint for fetching all the users

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
-import express from "express";
+import express from 'express';
+import 'dotenv/config';
+import { prisma } from "./lib/prisma"
 
 const app = express();
-
 app.use(express.json());
 
+app.get('/users', async (_, res) => {
+  const users = await prisma.user.findMany();
+  return res.status(200).json({users : users} )
+});
+
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
+  console.log('Server is running on port 3000');
+});
