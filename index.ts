610a2dbee2be3f3986a494ee76c10ee26593ac58
Dev Sharma <devsharma1683@gmail.com>
2025-12-11 10:59:46 +0530

    added endpoints of update and delete the user

--- index.ts
+++ index.ts
@@ -6,9 +6,51 @@ const app = express();
 app.use(express.json());
 
 app.get('/users', async (_, res) => {
-  const users = await prisma.user.findMany();
+  // const users = await prisma.user.findMany(); // fetching all the user
+  // const users = await prisma.user.findUnique({ // fetching on basis of unique field
+  //   where: { email = user.email }
+  // })
+  // const users = await prisma.user.findMany({
+  //   // where: { isMarried: false, age: { gte: 20 } } // both conditon should be true
+  //   where: { 
+  //     OR: [{ isMarried: false }, { age: { gte: 20 }}] // any of them can be true
+  //   }
+  // })
+  // const users = await prisma.user.findMany({
+  //   where: { 
+  //     nationality: { not: "pakistan" } // fetching all the user which are not from pakistan
+  //   } 
+  // })
+  const users = await prisma.user.findMany({
+    where: { 
+      nationality: { in : ["india", "russia"] } // fetching all the user which are from india and russia
+    } 
+  })
   return res.status(200).json({users : users} )
 });
+
+app.put("/users", async (_, res) => {
+  const updatedUser = await prisma.user.update({
+    where: { email: "test2@email.com" },
+    data: { age: 16, isMarried: true }
+  })
+  
+  return res.status(201).json({
+    messsage: "user is updated",
+    user : updatedUser
+  })
+})
+
+app.delete("/users", async (_, res) => {
+  const deletedUser = await prisma.user.delete({
+    where: { email: "test2@email.com" }
+  })
+  
+  return res.status(200).json({
+    messsage: "user is deleted",
+    user : deletedUser,
+  })
+})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
